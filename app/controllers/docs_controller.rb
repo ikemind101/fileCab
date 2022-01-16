f1a3d0688f3581e4240d7f5435b06ce869f402af
Isaac Ahern <Isaac.m.ahern@gmail.com>
2022-01-16 10:46:59 -0600

    clean up controller and add index

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -1,11 +1,11 @@
 class DocsController < ApplicationController
+  before_action :find_doc, only: [:show, :edit, :update, :destroy]
 
   def index
+    @docs = Doc.all.order("created_at DESC")  
   end
 
-  def show
-
-  end
+  def show; end
 
   def new
     @doc = Doc.new
@@ -36,10 +36,10 @@ class DocsController < ApplicationController
   private 
 
     def find_doc
-
+      @doc = Doc.find(params[:id])
     end
 
     def doc_params
-     params.require(:doc).permit(:title, :content)
+      params.require(:doc).permit(:title, :content)
     end
 end
