2db4eeef516d66d4857eda693c2ed6d325b03c2e
Isaac Ahern <Isaac.m.ahern@gmail.com>
2022-01-16 14:27:55 -0600

    change root of app for auth user

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   get 'welcome/index'
 
   resources :docs
+  authenticated :user do
+    root "docs#index", as: "authenticated_root"
+  end
 
   root 'welcome#index'
 end
